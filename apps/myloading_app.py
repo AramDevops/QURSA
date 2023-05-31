6ef0713844aeef2d065d721a8a094974edb41cf6
Aram <79871730+AramDevops@users.noreply.github.com>
2023-05-31 15:50:49 -0400

    Update myloading_app.py

--- apps/myloading_app.py
+++ apps/myloading_app.py
@@ -2,7 +2,7 @@ import time
 import streamlit as st
 from hydralit import HydraHeadApp
 from hydralit_components import HyLoader, Loaders
-
+import base64
 
 class MyLoadingApp(HydraHeadApp):
 
@@ -16,19 +16,43 @@ class MyLoadingApp(HydraHeadApp):
 
         try:
             app_title = ''
-            if hasattr(app_target,'title'):
-                st.markdown("<img style='width:20%' src='https://i.imgur.com/aHsbSZZ.gif'>", unsafe_allow_html=True)
+            if hasattr(app_target, 'title'):
+                file_qursa = open("resources/qursa.gif", "rb")
+                contents = file_qursa.read()
+                data_url = base64.b64encode(contents).decode("utf-8")
+                file_qursa.close()
+
                 app_title = app_target.title
 
-            if app_title == 'Future options 1...':
-                with HyLoader():
-                    app_target.run()
+            if app_title == 'Home':
+                file = open("resources/wd.gif", "rb")
+                image = file.read()
+                data = base64.b64encode(image).decode("utf-8")
+                file.close()
 
-            elif app_title == 'QURSA':
-                app_target.run()
-            else:
-                app_target.run()
+            if app_title == 'Shor Algorithm':
+                file = open("resources/VIcR.gif", "rb")
+                image = file.read()
+                data = base64.b64encode(image).decode("utf-8")
+                file.close()
 
+            if app_title == 'RSA Algorithm':
+                file = open("resources/boxi.gif", "rb")
+                image = file.read()
+                data = base64.b64encode(image).decode("utf-8")
+                file.close()
+
+            # Create the HTML markup for the images
+            image_html = f'''
+                
+                <div style="display: flex; align-items: center;"><img src="data:image/png;base64,{data_url}" style="height: 25vh;"></div>
+                <div style="display: flex; justify-content: center; align-items: center;"><img src="data:image/png;base64,{data}" style="height: 25vh;"></div>
+        
+            '''
+
+            # Display the images
+            st.markdown(image_html, unsafe_allow_html=True)
+            app_target.run()
 
 
         except Exception as e:
