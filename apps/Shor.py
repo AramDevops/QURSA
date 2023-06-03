6cce2621e77022d0adabb60f53215492450e7d5e
Aram <79871730+AramDevops@users.noreply.github.com>
2023-06-02 22:25:13 -0400

    Update Shor.py

--- apps/Shor.py
+++ apps/Shor.py
@@ -54,13 +54,13 @@ class CheatApp(HydraHeadApp):
 
         st.markdown("""
             <style>
-            .css-1v0mbdj .ebxwdo61 {
+            .css-1v0mbdj.ebxwdo61 {
                 display: flex;
                 justify-content: center;
                 align-items: center;
             }
 
-            .css-1v0mbdj .ebxwdo61 img {
+            .css-1v0mbdj.ebxwdo61 img {
                 max-height: 100%;
                 width: auto;
                 border: 2px solid #fff;
