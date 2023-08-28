8ea8fbbbb3dc7ae8693142b066f332d19364c440
Aram <79871730+AramDevops@users.noreply.github.com>
2023-08-27 20:59:36 -0400

    Update Shor.py

--- apps/Shor.py
+++ apps/Shor.py
@@ -417,6 +417,9 @@ class CheatApp(HydraHeadApp):
                                 factors.add(guess)
                                 ls_periods.append(r_val)
                                 n_a_val.append(n_value_a)
+
+                                st.write(n_a_val)
+                                st.write(ls_periods)
                                 
 
                         """ 
